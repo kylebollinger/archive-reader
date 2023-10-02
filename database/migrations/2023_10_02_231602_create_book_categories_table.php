dd8f9d2d668e740df159e7386faf43956e882544
Kyle Bollinger <kbollinger32@gmail.com>
2023-10-02 16:18:39 -0700

    Migration to create BookCategories table

---
+++ database/migrations/2023_10_02_231602_create_book_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255)->nullable();
+            $table->string('slug', 255)->nullable()->unique()->index();
+            $table->text('description')->nullable();
+            $table->json('data')->nullable();
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book_categories');
+    }
+};
