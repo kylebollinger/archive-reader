c2a39dafccbf16c034b83dbad4e19b78e31ef945
Kyle Bollinger <kbollinger32@gmail.com>
2023-10-03 01:46:25 -0700

    [bug fix] Update query to accomodate edge cases in book covers

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -13,7 +13,10 @@ class BooksController extends BaseController
 {
     public function index(Request $request)
     {
-        $books = Book::state('completed')->whereNotNull('cover')->inRandomOrder()->take(32)->get();
+        $books = Book::state('completed')
+                        ->where('cover', '!=', '')
+                        ->orwhere('cover', '!=', null)
+                        ->inRandomOrder()->take(32)->get();
         $categories = BookCategory::all()->where('book_count', '>=', 10);
         $abc_indicies = BookCategory::alphabetical_indicies();
         return view('books.index', compact('books', 'categories', 'abc_indicies'));
