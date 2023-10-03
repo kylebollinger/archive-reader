093897153d9ee0f4041390825193d9d90b35803b
Kyle Bollinger <kbollinger32@gmail.com>
2023-10-03 01:10:11 -0700

    [bug fix] Incorrect route name

--- resources/views/books/partials/category.blade.php
+++ resources/views/books/partials/category.blade.php
@@ -1,5 +1,5 @@
 <div class="grid-item category-link">
   <a  class="button button--secondary button--xl"
-      href="{{ route('explore.library.slug.router', $category->slug) }}"
+      href="{{ route('library.slug.router', $category->slug) }}"
       >{!! $category->name !!}</a>
 </div>
