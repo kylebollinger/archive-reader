efe0a3bf08c30ee8818af4902f9a5aae7e62538c
Kyle Bollinger <kbollinger32@gmail.com>
2023-10-08 11:29:34 -0700

    Add google analytics integration

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -10,4 +10,6 @@
 <link rel="mask-icon" href="https://archive-reader.s3.us-west-2.amazonaws.com/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
 <meta name="msapplication-TileColor" content="#da532c">
 <meta name="theme-color" content="#ffffff">
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9ZK6EEN92N"></script>
+<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-9ZK6EEN92N');</script>
 <script>(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:3706779,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');</script>
